9ebdd39883bf7e8de5af12586b220cd556e1dd50
nagisakyn04 <nagisakyn04@gmail.com>
2020-02-15 20:10:01 +0600

    first commit

---
+++ travel/cities/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import home ,  CityDetailView
+
+urlpatterns = [
+    path('detail/<int:pk>/', CityDetailView.as_view() , name='detail'),
+    path('', home,name='home'),
+
+]
